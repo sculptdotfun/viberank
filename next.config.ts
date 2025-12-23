db978a4937217362e5a319da2e13d9fa0110e873
KaitouKid1412 <vsurya.ayygari@gmail.com>
2025-12-23 16:09:50 +0530

    fix: dashboard fixes 4

--- next.config.ts
+++ next.config.ts
@@ -16,15 +16,7 @@ const nextConfig: NextConfig = {
     "http://35.175.206.198:3000",
     "http://35.175.206.198:3001"
   ],
-  allowedOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:3001", 
-    "http://127.0.0.1:3001",
-    "http://35.175.206.198",
-    "http://35.175.206.198:3000",
-    "http://35.175.206.198:3001"
-  ],
+
 };
 
 export default nextConfig;
